64ae02d48aa995f48053abae0e3d466f8dfdd059
CarloCassini <c.cassini87@gmail.com>
2023-11-30 22:22:15 +0100

    change apiurl in store.js

--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,6 @@ import { reactive } from "vue";
 // qui dentro mettimao tutti i data globali
 export const store = reactive({
   esempio: "valore di ciccio",
-  apiUrl:'http://127.0.0.1:8000/api',
+  // apiUrl:'http://127.0.0.1:8000/api',
+  apiUrl: "http://localhost:8000/api",
 });
