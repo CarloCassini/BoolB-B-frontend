7f4aa2fa63e58e6f6b35f818819baf05460f0008
CarloCassini <c.cassini87@gmail.com>
2023-11-30 22:34:24 +0100

    add sotreUrl to store - use it in apartmentCard

--- src/components/apartments/ApartmentCard.vue
+++ src/components/apartments/ApartmentCard.vue
@@ -1,10 +1,11 @@
 <script>
 // import MyComponent from "./components/MyComponent.vue";
-
+import { store } from "../../data/store";
 export default {
   data() {
     return {
-      title: "Card"
+      title: "Card",
+      store,
     }
   },
   props:{
@@ -48,7 +49,8 @@ export default {
   <div>
     <img v-if="!apartment.cover_image_path" src="https://via.placeholder.com/2000x1500.png/333333?text=Placeholder" class="card-img-top h-100" alt="Placeholder">
     <img v-else-if="apartment.cover_image_path.startsWith('http://') || apartment.cover_image_path.startsWith('https://')" :src="apartment.cover_image_path" class="card-img-top h-100" alt="External Image">
-    <img v-else :src="'http://127.0.0.1:8000/storage/' + apartment.cover_image_path" class="card-img-top h-100" alt="Uploaded Image"> 
+    <!-- <img v-else :src="'http://127.0.0.1:8000/storage/' + apartment.cover_image_path" class="card-img-top h-100" alt="Uploaded Image">  -->
+    <img v-else :src="store.storageUrl + apartment.cover_image_path" class="card-img-top h-100" alt="Uploaded Image"> 
   </div>
   <div class="card-body">
     <h5 class="card-title">{{apartment.title}}</h5>
--- src/data/store.js
+++ src/data/store.js
@@ -4,4 +4,5 @@ export const store = reactive({
   esempio: "valore di ciccio",
   // apiUrl:'http://127.0.0.1:8000/api',
   apiUrl: "http://localhost:8000/api",
+  storageUrl: "http://localhost:8000/storage/",
 });
