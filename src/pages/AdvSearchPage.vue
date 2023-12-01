596a22b9808d1cb2b3b29faab80a1be8d98256f9
CarloCassini <c.cassini87@gmail.com>
2023-12-01 11:28:17 +0100

    add mounted

--- src/pages/AdvSearchPage.vue
+++ src/pages/AdvSearchPage.vue
@@ -11,12 +11,11 @@ export default {
       title: "Search Your perfect Apartment",
       fetch: 0,
       services: [],
-      apartments:[],
-
+      apartments: [],
     };
   },
   components: {
-    ApartmentsList,/* FilterBoxUi, */
+    ApartmentsList /* FilterBoxUi, */,
   },
   methods: {
     // fetchApartments(apiUri = store.apiUrl + "/apartments/service/" + 1) {
@@ -28,118 +27,125 @@ export default {
     //     this.fetch++;
     //   });
     // },
-    fetchApartments(){
-        const activeServices=[]
-        let distance = 20
-        let beds = 1
-        let rooms = 1
-        const distanceEl = document.getElementById('distance')
-        const bedsEl = document.getElementById('beds')
-        const roomsEl = document.getElementById('rooms')
-        if (distanceEl.value) {
-            distance = distanceEl.value
-        }
-        if (bedsEl.value) {
-            beds = bedsEl.value
-        }
-        if (roomsEl.value) {
-            rooms = roomsEl.value
-        }
-        this.services.forEach((service) => {
-            if(service.active) activeServices.push(service.id)
-            
-        })
-    // console.log(activeServices,distance,beds,rooms);
-    
-    axios.post(store.apiUrl + '/apartments-by-filters',{activeServices,distance,beds,rooms},{
-        headers:{
-            'contet-type' : 'multypart/form-data'
-        }
-    }).then((response)=>{
-      
-      this.apartments = response.data.data
-    })
-    
+    fetchApartments() {
+      const activeServices = [];
+      let distance = 20;
+      let beds = 1;
+      let rooms = 1;
+      const distanceEl = document.getElementById("distance");
+      const bedsEl = document.getElementById("beds");
+      const roomsEl = document.getElementById("rooms");
+      if (distanceEl.value) {
+        distance = distanceEl.value;
+      }
+      if (bedsEl.value) {
+        beds = bedsEl.value;
+      }
+      if (roomsEl.value) {
+        rooms = roomsEl.value;
+      }
+      this.services.forEach((service) => {
+        if (service.active) activeServices.push(service.id);
+      });
+      // console.log(activeServices,distance,beds,rooms);
+
+      axios
+        .post(
+          store.apiUrl + "/apartments-by-filters",
+          { activeServices, distance, beds, rooms },
+          {
+            headers: {
+              "contet-type": "multypart/form-data",
+            },
+          }
+        )
+        .then((response) => {
+          this.apartments = response.data.data;
+        });
     },
-    fetchServices(){
-        axios.get(store.apiUrl + '/services').then((response) =>{
-            this.services = response.data.map((service) => {
-                return{
-                    ...service,
-                    active: false,
-                }
-            })
-        })
+    fetchServices() {
+      axios.get(store.apiUrl + "/services").then((response) => {
+        this.services = response.data.map((service) => {
+          return {
+            ...service,
+            active: false,
+          };
+        });
+      });
     },
-    toggleService(service){
-        service.active = !service.active
-        this.fetchApartments()
-    }
+    toggleService(service) {
+      service.active = !service.active;
+      this.fetchApartments();
+    },
   },
   created() {
     this.fetchServices();
-
-    // this.fetchApartments();
+  },
+  mounted() {
+    this.fetchApartments();
   },
 };
 </script>
 
 <template>
   <div class="container">
-
     <h1>{{ title }}</h1>
-    <hr>
+    <hr />
     <div class="card" data-bs-theme="light">
-    <div class="card-header text-center">
+      <div class="card-header text-center">
         <h5>Filters</h5>
+      </div>
+      <div class="card-body">
+        <span> services </span>
+        <span
+          v-for="(service, index) in this.services"
+          :class="{ disabled: !service.active }"
+          class="badge me-2 my-2 bg-gradient bg-dark fs-5"
+          @click="toggleService(service)"
+        >
+          <font-awesome-icon :icon="service.symbol" style="color: #ff7977" />
+        </span>
+        <hr />
+        <span> distance </span>
+        <input
+          @change="fetchApartments()"
+          type="number"
+          id="distance"
+          class="bg-light text-dark border"
+        />
+        <hr />
+        <span> beds </span>
+        <input
+          @change="fetchApartments()"
+          type="number"
+          id="beds"
+          class="bg-light text-dark border"
+        />
+        <hr />
+        <span> rooms </span>
+        <input
+          @change="fetchApartments()"
+          type="number"
+          id="rooms"
+          class="bg-light text-dark border"
+        />
+      </div>
+      <div class="card-footer"></div>
     </div>
-    <div class="card-body">
-    <span>
-        services
-    </span>
-    <span v-for="(service,index) in this.services" 
-    :class="{disabled: !service.active}" 
-    class="badge me-2 my-2 bg-gradient bg-dark fs-5"
-    @click="toggleService(service)">
-
-    <font-awesome-icon :icon="service.symbol" style="color:#ff7977;" />
-    </span>
-    <hr>
-    <span>
-        distance
-    </span>
-    <input @change="fetchApartments()" type="number" id="distance" class="bg-light text-dark border">
-    <hr>
-    <span>
-        beds
-    </span>
-    <input @change="fetchApartments()" type="number" id="beds" class="bg-light text-dark border">
-    <hr>
-    <span>
-        rooms
-    </span>
-    <input @change="fetchApartments()" type="number" id="rooms" class="bg-light text-dark border">
-    </div>
-    <div class="card-footer">
-    </div>
-    </div>
-   <!--  <FilterBoxUi /> -->
+    <!--  <FilterBoxUi /> -->
     <!-- <div @click="fetchApartments()" class="btn">ciccio</div> -->
     <div class="row">
       <div class="col-md-6">
         <!-- <ApartmentsList :apartments="this.apartments" :key="fetch" /> -->
-        <ApartmentsList :colNum=4 :apartments="this.apartments" />
+        <ApartmentsList :colNum="4" :apartments="this.apartments" />
       </div>
-      <div class="col-md-6">
-        mappa
-      </div>
+      <div class="col-md-6">mappa</div>
     </div>
   </div>
-
 </template>
 
 <style lang="scss" scoped>
-  .disabled{
-      filter: grayscale(1);
-  }
+.disabled {
+  filter: grayscale(1);
+}
 </style>
