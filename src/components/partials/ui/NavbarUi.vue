e068ff8e85ddc0884545385e11dc8aa52e17cea9
CarloCassini <c.cassini87@gmail.com>
2023-12-04 14:05:16 +0100

    add registrati/login to navbar

--- src/components/partials/ui/NavbarUi.vue
+++ src/components/partials/ui/NavbarUi.vue
@@ -6,69 +6,87 @@ import { store } from "../../../data/store.js";
 export default {
   data() {
     return {
-      store
-    }
-
+      store,
+    };
   },
 
-  components: {
-
-  },
-
+  components: {},
 };
 </script>
 
 <template>
-    
- <nav class="navbar navbar-expand-lg bg-gradient bg-light sticky-top" data-bs-theme="light">
-
-  <div class="container-fluid">
-    
-    <router-link :to="{ name: 'homepage' }" aria-current="page" class="navbar-brand">
-      
-      <img src="../../../img/airbnb-logo-small-center.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top logo-img">
+  <nav
+    class="navbar navbar-expand-lg bg-gradient bg-light sticky-top"
+    data-bs-theme="light"
+  >
+    <div class="container-fluid">
+      <router-link
+        :to="{ name: 'homepage' }"
+        aria-current="page"
+        class="navbar-brand"
+      >
+        <img
+          src="../../../img/airbnb-logo-small-center.png"
+          alt="Logo"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top logo-img"
+        />
         <span class="logo-txt ms-1">Boolbnb</span>
-    </router-link>
+      </router-link>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul
+          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+          style="--bs-scroll-height: 100px"
+        >
+          <li class="nav-item">
+            <router-link
+              :to="{ name: 'homepage' }"
+              aria-current="page"
+              class="nav-link"
+            >
+              Home
+            </router-link>
+          </li>
 
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <router-link :to="{ name: 'search' }" class="nav-link">
+              Search
+            </router-link>
+          </li>
 
-        <li class="nav-item">
-          
-          <router-link :to="{ name: 'homepage' }" aria-current="page" class="nav-link">
-            Home
-          </router-link>
-          
-        </li>
-        
-        <li class="nav-item">
-          
-          <router-link :to="{ name: 'search' }"  class="nav-link">
-            Search
-          </router-link>
-          
-        </li>
+          <li>
+            <a href="http://localhost:8000/login" class="nav-link"> login </a>
+          </li>
 
-      </ul>
+          <li>
+            <a href="http://localhost:8000/register" class="nav-link">
+              Registrati
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-<div>
- 
-</div>
-
-
+  </nav>
+  <div></div>
 </template>
 
 <style lang="scss" scoped>
-  .logo-txt{
-    color: #ff7977;
-    font-weight: 600;
-  }
-
+.logo-txt {
+  color: #ff7977;
+  font-weight: 600;
+}
 </style>
