39dadab0fdc3c0620df7b14934507aa00517521f
CarloCassini <c.cassini87@gmail.com>
2023-12-10 11:24:12 +0100

    change service color

--- src/components/partials/ui/HeadingTxtUi.vue
+++ src/components/partials/ui/HeadingTxtUi.vue
@@ -42,14 +42,6 @@ export default {
           />
         </div>
       </div>
-      <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button> -->
       <div class="my-5 txt">
         <h2 class="display-6 text-center" style="color: #ffffff">
           {{ subtitle }}
